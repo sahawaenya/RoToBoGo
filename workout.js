569e03be8bfdf057cb4189d3a5c496fdf490676f
regi <regiekaerviawan@gmail.com>
2022-07-21 19:15:09 +0700

    workout

---
+++ workout.js
@@ -0,0 +1,33 @@
+function workout(data) {
+const workout = {
+  dada : {
+    pushUp : 10,
+    plank : 1,
+    chestPress : 10,
+    pullUp : 10,
+    chairDips : 15
+  },
+  perut : {
+    sitUp : 10,
+    crunch : 10,
+    mountainClimber : 8,
+    plank : 1,
+    Bridhe : 1,
+  },
+  kaki : {
+    squat : 10,
+    jumpingJack : 11,
+    lunge : 14,
+    sitWall : 1,
+    burpee : 10
+  },
+  lengan : {
+    pushUp : 10,
+    plankPress : 10,
+    curlBarbel : 16,
+    burpee : 16,
+    shoulderPress: 12,
+  }
+  }
+}
+console.log(data);
