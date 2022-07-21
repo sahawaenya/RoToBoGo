4c8f5d25b18657979b9ae0925c25e6dfd571638c
Aji Purnomo <semlakune@gmail.com>
2022-07-21 22:07:21 +0700

    html

--- js/bmi.js
+++ js/bmi.js
@@ -26,59 +26,57 @@
 // ------------------------------------------------------------------------------------------------------------------------------
 // === input ===
 
+// let weight = document.getElementById("weight").value
+// let height = document.getElementById("height").value
 
 
 // === function ===
 
-function calculateBmi() {
-    
-    let height = document.getElementById("height").value
-    let weight = document.getElementById("weight").value
+function calculateBmi(height, weight) {
+
     let result = 0
-    let bmi = null
     height /= 100
     result = weight / Math.pow(height, 2)
 
-    result = result.toFixed(1)
-    
-    bmi = result
-    let message = null
-    let desc = null
-    
-    if (bmi <= 18.4) {
-        message = `BMI Anda adalah ${bmi} Anda kurang berat badan`
-        desc = `kurus`
-    } else if (bmi >= 18.5 && bmi <= 24.9) {
-        message = `BMI Anda adalah ${bmi} BMI Anda termasuk normal`
-        desc = `normal`
-    } else if (bmi >= 25 && bmi <= 29.9) {
-        message = `BMI Anda adalah ${bmi} Anda tergolong gemuk`
-        desc = `gemuk`
-    } else if (bmi >= 30) {
-        message = `BMI Anda adalah ${bmi} Anda obesitas`
-        desc = `obes`
-    }
-    
-    document.getElementById("005result").innerText = bmi
-    document.getElementById("005message").innerText = message
-    
-    if (desc === 'kurus') {
-        document.getElementById('005kurus').style.fontWeight = "bold"
-        document.getElementById("005wise").innerText = 'Hai Kamu! Jangan bersedih, setiap sesuatu yang besar berawal dari yang kecil. Tingkatkan terus dan jaga pola makan, serta makan makanan yang sehat agar berat badanmu segera menuju ideal yaa'
-    } else if (desc === 'normal') {
-        document.getElementById('005normal').style.fontWeight = "bold"
-        document.getElementById("005wise").innerText = 'Selamat! berat badanmu ideal! jaga selalu kesehatan tubuhmu dengan menjaga pola makan dan rajin berolahraga'
-    } else if (desc === 'gemuk') {
-        document.getElementById('005gemuk').style.fontWeight = "bold"
-        document.getElementById("005wise").innerText = 'Hai Kamu! Jangan bersedih, kamu hanya perlu sedikit berusaha agar berat badanmu berkurang. Rajin berolahraga dan jaga pola makan yaa'
-    } else if (desc === 'obes') {
-        document.getElementById('005obes').style.fontWeight = "bold"
-        document.getElementById("005wise").innerText = 'Hai Kamu! Jangan bersedih, kamu tidak sendiri kok. Mari kita perbaiki bersama-sama dengan dimulai dari kebiasaan hidup sehat yaa'
-    }
-    
-    
+    return result.toFixed(1)
 }
 
 // console.log(calculateBmi(173, 83));
 
 // == dom ==
+
+const bmi = calculateBmi(173, 50)
+let message = null
+let desc = null
+
+if (bmi <= 18.4) {
+    message = `BMI Anda adalah ${bmi} Anda kurang berat badan`
+    desc = `kurus`
+} else if (bmi >= 18.5 && bmi <= 24.9) {
+    message = `BMI Anda adalah ${bmi} BMI Anda termasuk normal`
+    desc = `normal`
+} else if (bmi >= 25 && bmi <= 29.9) {
+    message = `BMI Anda adalah ${bmi} Anda tergolong gemuk`
+    desc = `gemuk`
+} else if (bmi >= 30) {
+    message = `BMI Anda adalah ${bmi} Anda obesitas`
+    desc = `obes`
+}
+
+if (desc === 'kurus') {
+    document.getElementById('005kurus').style.fontWeight = "bold"
+    document.getElementById("005wise").innerText = 'Hai Kamu! Jangan bersedih, setiap sesuatu yang besar berawal dari yang kecil. Tingkatkan terus dan jaga pola makan, serta makan makanan yang sehat agar berat badanmu segera menuju ideal yaa'
+} else if (desc === 'normal') {
+    document.getElementById('005normal').style.fontWeight = "bold"
+    document.getElementById("005wise").innerText = 'Selamat! berat badanmu ideal! jaga selalu kesehatan tubuhmu dengan menjaga pola makan dan rajin berolahraga'
+} else if (desc === 'gemuk') {
+    document.getElementById('005gemuk').style.fontWeight = "bold"
+    document.getElementById("005wise").innerText = 'Hai Kamu! Jangan bersedih, kamu hanya perlu sedikit berusaha agar berat badanmu berkurang. Rajin berolahraga dan jaga pola makan yaa'
+} else if (desc === 'obes') {
+    document.getElementById('005obes').style.fontWeight = "bold"
+    document.getElementById("005wise").innerText = 'Hai Kamu! Jangan bersedih, kamu tidak sendiri kok. Mari kita perbaiki bersama-sama dengan dimulai dari kebiasaan hidup sehat yaa'
+}
+
+
+document.getElementById("005result").innerText = bmi
+document.getElementById("005message").innerText = message
